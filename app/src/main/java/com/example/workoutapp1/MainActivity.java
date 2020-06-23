f8fe5f9c61e4df7ffc54ac503d5a0fb9781a4443
SongRe <songks712@gmail.com>
2020-06-23 17:56:20 -0400

    Button now connects to MainScreen activity

--- app/src/main/java/com/example/workoutapp1/MainActivity.java
+++ app/src/main/java/com/example/workoutapp1/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.workoutapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.startButton);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainScreen();
+            }
+        });
+    }
+
+    public void openMainScreen() {
+        Intent intentOpenMainScreen = new Intent(this, MainScreen.class);
+        startActivity(intentOpenMainScreen);
     }
 }

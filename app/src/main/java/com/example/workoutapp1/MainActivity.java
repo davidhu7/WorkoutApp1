52be8bee3c903927aac5733a1a75d6b984135097
SongRe <songks712@gmail.com>
2020-06-23 21:06:58 -0400

    statr and save buttons

--- app/src/main/java/com/example/workoutapp1/MainActivity.java
+++ app/src/main/java/com/example/workoutapp1/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button savedBtn;
     private Button startBtn;
 
     @Override
@@ -15,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBtn = (Button) findViewById(R.id.startNewButton);
+        startBtn = findViewById(R.id.startNewButton);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openMainScreen();
+            }
+        });
+
+        savedBtn = (Button) findViewById(R.id.savedButton);
+        savedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSavedScreen();
             }
 
 
@@ -29,5 +38,10 @@ public class MainActivity extends AppCompatActivity {
     public void openMainScreen() {
         Intent intentOpenMainScreen = new Intent(this, NewWorkoutActivity.class);
         startActivity(intentOpenMainScreen);
+    }
+
+    public void openSavedScreen() {
+        Intent intentOpenSavedScreen = new Intent(this, SavedWorkoutsActivity.class);
+        startActivity(intentOpenSavedScreen);
     }
 }
--- app/src/main/java/com/example/workoutapp1/SavedWorkoutsActivity.java
+++ app/src/main/java/com/example/workoutapp1/SavedWorkoutsActivity.java
@@ -1,14 +1,71 @@
 package com.example.workoutapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class SavedWorkoutsActivity extends AppCompatActivity {
+
+    private LinearLayout sLinearLayout;
+    private ArrayList<CardView> cardViews;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saved_workouts);
+        sLinearLayout = findViewById(R.id.viewList);
+
+        CardView card = new CardView(this);
+
+        // Set the CardView layoutParams
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        card.setLayoutParams(params);
+
+        // Set CardView corner radius
+        card.setRadius(9);
+
+        // Set cardView content padding
+        card.setContentPadding(15, 15, 15, 15);
+
+        // Set a background color for CardView
+        card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+        // Set the CardView maximum elevation
+        card.setMaxCardElevation(15);
+
+        // Set CardView elevation
+        card.setCardElevation(9);
+
+        // Initialize a new TextView to put in CardView
+        TextView tv = new TextView(this);
+        tv.setLayoutParams(params);
+        tv.setText("CardView\nProgrammatically");
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+        tv.setTextColor(Color.RED);
+
+        // Put the TextView in CardView
+        card.addView(tv);
+
+
+        sLinearLayout.addView(card);
+
     }
 }

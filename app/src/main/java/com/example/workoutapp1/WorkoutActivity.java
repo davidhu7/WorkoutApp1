e0d41ef1f31375d7e00f0ce73168e44765313778
ksong200 <hud1945@gmail.com>
2020-06-28 22:55:10 -0400

    fixed the display to show exercises correctly

--- app/src/main/java/com/example/workoutapp1/WorkoutActivity.java
+++ app/src/main/java/com/example/workoutapp1/WorkoutActivity.java
@@ -6,8 +6,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,15 +14,19 @@ import android.widget.Toast;
 public class WorkoutActivity extends AppCompatActivity {
     private CountDownTimer countDownTimer;
     private TextView countdown;
+    private TextView exercise;
     private Button start_pause;
     private boolean timerRunning;
     private long timeLeft;
-
+    private int isSet;
     private int workTime;
     private int restTime;
     private int cooldownTime;
     private boolean isWork;
     private int counter;
+    private int exerciseNum;
+    private int sets;
+    private int cycles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +34,23 @@ public class WorkoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_workout);
 
         Intent getWorkout = getIntent();
-        Workout newWorkout = getWorkout.getParcelableExtra(NewWorkoutActivity.EXTRA_WORKOUT);
+        Workout newWorkout = (Workout)getWorkout.getParcelableExtra(NewWorkoutActivity.EXTRA_WORKOUT);
 
         workTime = newWorkout.getWorkTime();
         restTime = newWorkout.getRestTime();
         cooldownTime = newWorkout.getCooldownTime();
-        final int sets = newWorkout.getSets();
-        final int cycles = newWorkout.getCycles();
+        sets = newWorkout.getSets();
+        cycles = newWorkout.getCycles();
         countdown = findViewById(R.id.Timer);
+        exercise = findViewById(R.id.Exercise);
         start_pause = findViewById(R.id.Start_Pause);
         timeLeft = workTime * 1000;
         updateTime();
         isWork = true;
         counter = sets * cycles;
+        exerciseNum = 1;
+        isSet = 1;
+        exercise.setText("Exercise " + exerciseNum);
 
         start_pause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +67,6 @@ public class WorkoutActivity extends AppCompatActivity {
 
 
     }
-
 
     public void TotalWorkout(int workT, int restT, int cooldownT, int setsNum, int cyclesNum){
         workT = workT * 1000;
@@ -102,11 +107,31 @@ public class WorkoutActivity extends AppCompatActivity {
                     } else {
                         timeLeft = workTime * 1000;
                         isWork = true;
+                        if(counter-1 > 0) {
+                            if(isSet < sets) {
+                                exerciseNum++;
+                                exercise.setText("Exercise " + exerciseNum);
+                                isSet++;
+                            }
+                            else{
+                                isSet = 1;
+                                exerciseNum = 1;
+                                exercise.setText("Exercise " + exerciseNum);
+                            }
+                        }
                         counter--;
                     }
+
                     startTime();
                 }
 
+                if(counter == 0){
+                    timeLeft = cooldownTime * 1000;
+                    exercise.setText("Cooldown");
+                    updateTime();
+                    startTime();
+                    counter--;
+                }
 
 
 

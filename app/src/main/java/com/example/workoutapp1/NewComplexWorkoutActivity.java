bddcbe6393447460298ee922b47b2b47d0d2fc0b
SongRe <songks712@gmail.com>
2020-07-29 22:28:00 -0400

    Created activity and layout for newComplexWorkoutActivity

---
+++ app/src/main/java/com/example/workoutapp1/NewComplexWorkoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.workoutapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewComplexWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_complex_workout);
+    }
+}
